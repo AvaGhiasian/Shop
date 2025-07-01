1a22963da0e87a394424aec085ed652feef8d548
Ava Ghiasian <avaghiasian82@gmail.com>
2025-07-01 11:14:31 +0330

    added order model

---
+++ orders/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+
+from store.models import Product
+
+
+# Create your models here.
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=100)
+    address = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=10)
+    province = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+
+        verbose_name = 'سفارش'
+        verbose_name_plural = 'سفارش ها'
+
+    def __str__(self):
+        return f"order {self.id}"
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+    def get_post_cost(self):
+        weight = sum(item.get_weight() for item in self.items.all())
+        if weight < 1000:
+            return 0
+        elif 1000 <= weight <= 2000:
+            return 0
+        else:
+            return 0
+
+    def get_final_cost(self):
+        price = self.get_post_cost() + self.get_total_cost()
+        return price
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name="order_items", on_delete=models.CASCADE)
+    price = models.PositiveIntegerField(default=0)
+    quantity = models.PositiveIntegerField(default=1)
+    weight = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'آیتم سفارش'
+        verbose_name_plural = 'آیتم های سفارش'
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
+
+    def get_weight(self):
+        return self.weight * self.quantity
